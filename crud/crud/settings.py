efc2e82bae3abe1f46cefa219fc69a1048701b9b
Fayaz409 <ftunio06@gmail.com>
2025-04-01 18:54:13 +0500

    Connected with MySQL and cretaed a new Employee Table

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+        'NAME'    : 'cruddb',                 # <-- UPDATED line 
+        'USER'    : 'crud',                     # <-- UPDATED line
+        'PASSWORD': 'Secret_1234',              # <-- UPDATED line
+        'HOST'    : 'localhost',                # <-- UPDATED line
+        'PORT'    : '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- crud/crudapp/models.py
+++ crud/crudapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    Countries =[
+        ('India','India'),
+        ('USA','USA'),
+        ('UK','UK'),
+        ('Germany','Germany'),
+        ('France','France'),
+        ('Italy','Italy'),
+        ('Spain','Spain'),
+        ('Japan','Japan'),
+        ('China','China'),
+        ('Russia','Russia')
+    ]
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    Title = models.CharField(max_length=50)
+    HasPassport = models.BooleanField(default=False)
+    Salary = models.IntegerField()
+    DateOfBirth = models.DateField()
+    HireDate = models.DateField()
+    Notes = models.CharField(max_length=200)
+    Country= models.CharField(max_length=50,choices=Countries,default=None)
+
